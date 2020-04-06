a7189c891d1a5ee359e5d2ab3dfa993a996aa2bd
George Constantinou <georgec20001@gmail.com>
2020-04-06 19:26:19 +0300

    Added RPC client namespaced exceptions

---
+++ src/JsonRPC/PayloadException.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Copyright (c) AlleoChain Ltd. (https://alleochain.com)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) AlleoChain Ltd. (https://alleochain.com)
+ * @license       https://opensource.org/licenses/mit-license.php MIT License
+ */
+
+namespace AlleoChain\Multichain\JsonRPC;
+
+final class PayloadException extends \InvalidArgumentException implements ClientException
+{
+}
